0198d1aea3c4bdcb31b3a1d719361ef39dbe0fa7
Damien <damien.postlethwaite@gmail.com>
2011-02-18 23:22:11 +0000

    Modified Test.java

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Test
+		
+		
+		System.out.println("This is the first change to the java file before committing");
 	}
 
 }
